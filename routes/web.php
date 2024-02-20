0af7bafbce21af7970a9589503f30a4dd1b91a25
Ren0628 <ren6nt@gmail.com>
2024-02-21 04:23:13 +0900

    bill.storeの追加

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BillController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('pages.index');
+});
+
+Route::get('create', [BillController::class, 'create']);
+
+Route::post('store', [BillController::class, 'store'])->name('bill.store');
