6368660a848f893a0c3314ac58158f210cf71eec
Ren0628 <ren6nt@gmail.com>
2024-02-24 08:30:41 +0900

    オートフィル実装

---
+++ public/js/create-form.js
@@ -0,0 +1,55 @@
+let issuerFormInput = document.getElementById('issuer');
+let receiverFormInput = document.getElementById('receiver');
+let paymentAddressInput = document.getElementById('payment_address');
+let paymentPlaceInput = document.getElementById('payment_place');
+let amountFormInput = document.getElementById('amount');
+
+let isEmpty = (obj) => {
+
+    return Object.keys(obj).length === 0
+}
+
+let toHalfWidthStr = (str) => {
+
+    str = str.replace(/[０-９]/g, (s)=>{
+        return String.fromCharCode(s.charCodeAt(0) - 0xFEE0);
+    });
+
+    return str;
+}
+
+issuerFormInput.addEventListener('change', (e) => {
+    
+    fetch('http://localhost/laravel-billbook-app/public/api/issuerdata',{
+        method:'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            issuer: e.target.value
+        }),
+    })
+        .then((response) => response.json())
+        .then((res) => {
+            
+            if(!isEmpty(res)) {
+                receiverFormInput.value = res.receiver;
+                paymentAddressInput.value = res.payment_address;
+                paymentPlaceInput.value = res.payment_place;
+            } else {
+                receiverFormInput.value = '';
+                paymentAddressInput.value = '';
+                paymentPlaceInput.value = '';
+            }
+        })
+        .catch((e) => {
+            console.log(e);
+        });
+});
+
+amountFormInput.addEventListener('change', (e) => {
+    
+    strAmount = new String(toHalfWidthStr(e.target.value)).replace(/,|￥/g, '');
+    
+    amountFormInput.value = Number(strAmount).toLocaleString('jp',{style:'currency',currency:'JPY'});
+});
